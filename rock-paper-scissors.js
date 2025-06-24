668b47660be34383a779bc0e66e0592aaf5024cd
kp-pham <170148118+kp-pham@users.noreply.github.com>
2025-06-24 10:52:38 -0700

    feat: increment human or computer score based on round winner
    Create capitalize function to capitalize human and computer choice when displaying round winner.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -17,3 +17,22 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("rock, paper, or scissors?");
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper") {
+        ++humanScore;
+        console.log(`You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`);
+    }
+    else {
+        ++computerScore;
+        console.log(`You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`);
+    }
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1); 
+}
